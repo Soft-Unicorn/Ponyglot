fc1e64c5789c5f734cbe9c632a38c5a3e3cb5e6a
Damien Braillard <contact@damien-braillard.ch>
2025-12-19 18:54:40 +0100

    Base project

---
+++ test/Ponyglot.Tests/DummyTest.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace Ponyglot.Tests;
+
+public class DummyTest
+{
+    [Fact]
+    public void TestNothing()
+    {
+        // Fake test so that "dotnet test" succeeds 
+    }
+}
