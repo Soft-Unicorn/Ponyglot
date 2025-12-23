9a4781e0ea61d15be4488009b8025eb6649b8cb4
Damien Braillard <contact@damien-braillard.ch>
2025-12-23 20:52:40 +0100

    Initial interfaces

---
+++ src/Ponyglot/ICultureSource.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace Ponyglot;
+
+/// <summary>
+/// Provides the current UI culture used for translation lookups.
+/// </summary>
+public interface ICultureSource
+{
+    /// <summary>
+    /// The culture to use to provide the translations.
+    /// </summary>
+    CultureInfo Culture { get; set; }
+}
---
+++ src/Ponyglot/ITranslationStore.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace Ponyglot;
+
+/// <summary>
+/// The store that provides the translations.
+/// </summary>
+public interface ITranslationStore
+{
+    /// <summary>
+    /// Tries to find a translated value for the given message key.
+    /// </summary>
+    /// <param name="domain">The domain (catalog) to look up.</param>
+    /// <param name="culture">The requested UI culture (implementations should apply parent fallback).</param>
+    /// <param name="context">The optional context to use when searching for translations provided by this instance.</param>
+    /// <param name="messageId">The message to translate.</param>
+    /// <returns>The translated string or <c>null</c> if no suitable translation is found.</returns>
+    string? Find(string domain, CultureInfo culture, string? context, string messageId);
+
+    /// <summary>
+    /// Tries to find a translated plural value for the given message key.
+    /// </summary>
+    /// <param name="domain">The domain (catalog) to look up.</param>
+    /// <param name="culture">The requested UI culture (implementations should apply parent fallback).</param>
+    /// <param name="context">The optional context to use when searching for translations provided by this instance.</param>
+    /// <param name="count">The count used for plural selection.</param>
+    /// <param name="messageId">The message to translate.</param>
+    /// <param name="pluralId">The plural message to translate.</param>
+    /// <returns>The translated string or <c>null</c> if no suitable translation is found.</returns>
+    string? FindPlural(string domain, CultureInfo culture, string? context, long count, string messageId, string pluralId);
+}
