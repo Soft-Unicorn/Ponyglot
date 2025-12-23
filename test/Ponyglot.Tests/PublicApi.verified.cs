2f197c10e140bdb5660ef9eb997fc0f30353ef5e
Damien Braillard <contact@damien-braillard.ch>
2025-12-23 20:54:20 +0100

    Add test to verify the public API

---
+++ test/Ponyglot.Tests/PublicApi.verified.cs
@@ -0,0 +1,29 @@
+﻿[assembly: System.CLSCompliant(true)]
+[assembly: System.Reflection.AssemblyMetadata("RepositoryUrl", "https://github.com/Soft-Unicorn/Ponyglot")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Ponyglot.Tests")]
+[assembly: System.Runtime.Versioning.TargetFramework(".NETStandard,Version=v2.0", FrameworkDisplayName=".NET Standard 2.0")]
+namespace Ponyglot
+{
+    public interface ICultureSource
+    {
+        System.Globalization.CultureInfo Culture { get; set; }
+    }
+    public interface ITranslationStore
+    {
+        string? Find(string domain, System.Globalization.CultureInfo culture, string? context, string messageId);
+        string? FindPlural(string domain, System.Globalization.CultureInfo culture, string? context, long count, string messageId, string pluralId);
+    }
+    public interface ITranslator
+    {
+        string? Context { get; }
+        string Domain { get; }
+        Ponyglot.ITranslator ForCulture(System.Globalization.CultureInfo culture);
+        string N(long count, string messageId, string pluralId, params object[]? args);
+        string T(string messageId, params object[]? args);
+    }
+    public interface ITranslatorFactory
+    {
+        Ponyglot.ITranslator Create(string domain, string? context);
+        Ponyglot.ITranslator Create<T>();
+    }
+}
---
+++ test/Ponyglot.Tests/PublicApiTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using PublicApiGenerator;
+using VerifyXunit;
+using Xunit;
+
+namespace Ponyglot.Tests;
+
+public class PublicApiTest
+{
+    [Fact]
+    public async Task PublicApi_SinceLastAccept_HasNotChanged()
+    {
+        // Arrange
+        var assembly = typeof(ITranslator).Assembly;
+
+        // Act
+        var publicApi = assembly.GeneratePublicApi().ReplaceLineEndings("\n");
+
+        // Assert
+        await Verifier
+            .Verify(publicApi, extension: "cs")
+            .UseFileName("PublicApi");
+    }
+}
