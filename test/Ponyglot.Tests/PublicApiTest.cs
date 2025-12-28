22049bc4055e7eeb1da7f195fec3cb05b568e605
Damien Braillard <contact@damien-braillard.ch>
2025-12-28 17:52:35 +0100

    Add test to verify the public API

---
+++ test/Ponyglot.Tests/PublicApiTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.Versioning;
+using System.Threading.Tasks;
+using PublicApiGenerator;
+using VerifyXunit;
+using Xunit;
+
+namespace Ponyglot.Tests;
+
+public class PublicApiTest
+{
+    [Fact]
+    public async Task PublicApi_SinceLastAccept_HasNotChanged()
+    {
+        // Arrange
+        var assembly = typeof(ITranslator).Assembly;
+
+        var options = new ApiGeneratorOptions
+        {
+            ExcludeAttributes = [typeof(TargetFrameworkAttribute).FullName!],
+        };
+
+        // Act
+        var publicApi = assembly.GeneratePublicApi(options).Replace(Environment.NewLine, "\n");
+
+        // Assert
+        await Verifier
+            .Verify(publicApi, extension: "cs")
+            .UseFileName("PublicApi");
+    }
+}
