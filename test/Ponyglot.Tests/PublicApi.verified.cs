c017b3972499f35df689ac43876d7871d835bd58
Damien Braillard <contact@damien-braillard.ch>
2025-12-28 17:52:35 +0100

    Add test to verify the public API

---
+++ test/Ponyglot.Tests/PublicApi.verified.cs
@@ -0,0 +1,28 @@
+﻿[assembly: System.CLSCompliant(true)]
+[assembly: System.Reflection.AssemblyMetadata("RepositoryUrl", "https://github.com/Soft-Unicorn/Ponyglot")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Ponyglot.Tests")]
+namespace Ponyglot
+{
+    public interface ICultureSource
+    {
+        System.Globalization.CultureInfo Culture { get; set; }
+    }
+    public interface ITranslationStore
+    {
+        bool TryGet(string domain, System.Globalization.CultureInfo culture, string context, string messageId, [System.Diagnostics.CodeAnalysis.NotNullWhen(true)] out string? translation);
+        bool TryGetPlural(string domain, System.Globalization.CultureInfo culture, string context, long count, string messageId, [System.Diagnostics.CodeAnalysis.NotNullWhen(true)] out string? translation);
+    }
+    public interface ITranslator
+    {
+        string Context { get; }
+        string Domain { get; }
+        Ponyglot.ITranslator ForCulture(System.Globalization.CultureInfo culture);
+        string N(long count, string messageId, string pluralId, params object?[]? args);
+        string T(string messageId, params object?[]? args);
+    }
+    public interface ITranslatorFactory
+    {
+        Ponyglot.ITranslator Create(string domain, string context);
+        Ponyglot.ITranslator Create<T>();
+    }
+}
