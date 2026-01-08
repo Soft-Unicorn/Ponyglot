51d7ca0634b5eef58b943b26cdc003f419e9a65d
Damien Braillard <contact@damien-braillard.ch>
2026-01-08 17:46:57 +0100

    TranslatorFactory

---
+++ src/Ponyglot/ITranslatorFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace Ponyglot;
+
+/// <summary>
+/// Factory used to create <see cref="ITranslator"/> instances.
+/// </summary>
+public interface ITranslatorFactory
+{
+    /// <summary>
+    /// Creates a new <see cref="ITranslator"/> instance with an explicit <paramref name="domain"/> and <paramref name="context"/>.
+    /// </summary>
+    /// <param name="domain">The domain that identifies the catalog to look up.</param>
+    /// <param name="context">The context to use when searching for translations.</param>
+    /// <returns>A new <see cref="ITranslator"/> instance.</returns>
+    ITranslator Create(string domain, string context);
+}
