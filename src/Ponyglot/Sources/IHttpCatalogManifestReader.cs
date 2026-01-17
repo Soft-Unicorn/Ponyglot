b2302296e150b55b394459b5926d1b72053dba8d
Damien Braillard <contact@damien-braillard.ch>
2026-01-17 18:14:23 +0100

    HTTP source

---
+++ src/Ponyglot/Sources/IHttpCatalogManifestReader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace Ponyglot.Sources;
+
+/// <summary>
+/// Reads a list of catalog URLs from an HTTP manifest.
+/// </summary>
+public interface IHttpCatalogManifestReader
+{
+    /// <summary>
+    /// Returns the list of media types handled by this reader.
+    /// </summary>
+    public IEnumerable<string> MediaTypes { get; }
+
+    /// <summary>
+    /// Reads the list of catalog URLs from the specified <paramref name="stream"/>.
+    /// </summary>
+    /// <param name="stream">The <see cref="Stream"/> to read from.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> that can be used to cancel the operation.</param>
+    /// <returns>A collection of raw unparsed uri.</returns>
+    IAsyncEnumerable<string> ReadAsync(Stream stream, CancellationToken cancellationToken = default);
+}
---
+++ test/Ponyglot.Tests/Sources/HttpCatalogSourceOptionsTest.cs
@@ -0,0 +1,84 @@
+﻿using AwesomeAssertions;
+using Ponyglot.Sources;
+using Xunit;
+
+namespace Ponyglot.Tests.Sources;
+
+public class HttpCatalogSourceOptionsTest
+{
+    private readonly HttpCatalogSourceOptions _sut = new();
+
+    [Fact]
+    public void ManifestReaders_Created_ShouldContainTheDefaultReaders()
+    {
+        // Arrange
+
+        // Act
+        var value = _sut.ManifestReaders;
+
+        // Assert
+        value.Should().SatisfyRespectively(
+            first => first.Should().BeOfType<HttpCatalogTextManifestReader>(),
+            second => second.Should().BeOfType<HttpCatalogJsonManifestReader>());
+    }
+
+    [Fact]
+    public void SameOrigin_Created_ShouldBeNull()
+    {
+        // Arrange
+
+        // Act
+        var value = _sut.SameOrigin;
+
+        // Assert
+        value.Should().BeTrue();
+    }
+
+    [Fact]
+    public void MaxCatalogs_Created_ShouldBeNull()
+    {
+        // Arrange
+
+        // Act
+        var value = _sut.MaxCatalogs;
+
+        // Assert
+        value.Should().Be(1000);
+    }
+
+    [Fact]
+    public void Filter_Created_ShouldBeNull()
+    {
+        // Arrange
+
+        // Act
+        var value = _sut.Filter;
+
+        // Assert
+        value.Should().BeNull();
+    }
+
+    [Fact]
+    public void ConfigureManifestRequest_Created_ShouldBeNull()
+    {
+        // Arrange
+
+        // Act
+        var value = _sut.ConfigureManifestRequest;
+
+        // Assert
+        value.Should().BeNull();
+    }
+
+    [Fact]
+    public void ConfigureCatalogRequest_Created_ShouldBeNull()
+    {
+        // Arrange
+
+        // Act
+        var value = _sut.ConfigureCatalogRequest;
+
+        // Assert
+        value.Should().BeNull();
+    }
+}
